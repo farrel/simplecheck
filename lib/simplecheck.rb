012c0c3824310f2b0884312ce5cef1fa4a2cb5e0
Farrel Lifson <farrel.lifson@aimred.com>
2013-11-04 13:26:39 +0200

    Private method renaming

--- lib/simplecheck.rb
+++ lib/simplecheck.rb
@@ -4,50 +4,50 @@ require 'simplecheck/check_failed'
 module Simplecheck
   def check( *arguments, &block )
     error_message = if block_given?
-                      __check_arguments_with_block( arguments, block )
+                      simplecheck_check_arguments_with_block( arguments, block )
                     else
-                      __check_arguments( arguments )
+                      simplecheck_check_arguments( arguments )
                     end
 
     if error_message
-      __handle_failure( error_message )
+      simplecheck_handle_failure( error_message )
     else
-      __handle_return_arguments( arguments )
+      simplecheck_handle_return_arguments( arguments )
     end
   end
 
 
   private
-  def __check_arguments( arguments )
+  def simplecheck_check_arguments( arguments )
     case arguments.size
     when 1
-      __check_expression( arguments[ 0 ])
+      simplecheck_check_expression( arguments[ 0 ])
     else
-      __check_case_equality( *arguments )
+      simplecheck_check_case_equality( *arguments )
     end
   end
 
-  def __check_arguments_with_block( arguments, block )
-    __check_arguments(( arguments + [ block ]))
+  def simplecheck_check_arguments_with_block( arguments, block )
+    simplecheck_check_arguments(( arguments + [ block ]))
   end
 
-  def __check_expression( expression )
+  def simplecheck_check_expression( expression )
     if !expression
       'Condition is not true' 
     end
   end
 
-  def __check_case_equality( *arguments, receiver )
+  def simplecheck_check_case_equality( *arguments, receiver )
     if invalid_argument = arguments.find{ |argument| !( receiver === argument )}
       "#{ invalid_argument } does not satisfy #{ receiver }" 
     end
   end
 
-  def __handle_failure( message )
+  def simplecheck_handle_failure( message )
     raise Simplecheck::CheckFailed.new( message )
   end
 
-  def __handle_return_arguments( arguments )
+  def simplecheck_handle_return_arguments( arguments )
     case arguments.size
     when 1
       arguments[0]
