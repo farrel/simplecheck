4365f3b6a39b780ae80533702cb5036614a5480a
Farrel Lifson <farrel.lifson@aimred.com>
2013-11-12 16:48:10 +0200

    Tests refactor

--- test/simplecheck_test.rb
+++ test/simplecheck_test.rb
@@ -1,98 +1,61 @@
 require 'test_helper'
 
-class Foo
+class TestSimplecheck < MiniTest::Test
   include Simplecheck
 
-  def expression_check( a )
-    check( a )
-  end
-
-  def case_equality_check( a )
-    check( a, Integer )
-  end
-
-  def case_equality_check_multiple_arguments( *arguments )
-    check( *arguments, Integer )
-  end
-
-  def block_check( a )
-    check( a ){ |x| x.modulo( 2 ).zero? }
-  end
-
-  def block_check_multiple_arguments( a, b, c )
-    check( a, b, c ){ |x| x.even? }
-  end
-
-  def lambda_argument_check( a )
-    check_lambda = lambda{ |x| x.even? }
-    check( a, check_lambda ) 
-  end
-
-  def lambda_block_check( a )
-    check_lambda = lambda{ |x| x.even? }
-    check( a, &check_lambda ) 
-  end
-end
-
-class TestSimplecheck < MiniTest::Test
-
-  def setup
-    @foo = Foo.new
-  end
-
   def test_expression_check_true
-    assert( @foo.expression_check( 1 ))
+    assert( check( 1 ))
   end
 
   def test_expression_check_raises_exception
-    assert_raises( Simplecheck::CheckFailed ){ @foo.expression_check( nil )}
+    assert_raises( Simplecheck::CheckFailed ){ check( nil )}
   end
 
   def test_case_equality_check_true
-    assert( @foo.case_equality_check( 1 ))
+    assert( check( 1, Integer ))
   end
 
   def test_case_equality_check_raises_exception
-    assert_raises( Simplecheck::CheckFailed ){ @foo.case_equality_check( '1' )}
+    assert_raises( Simplecheck::CheckFailed ){ check( '1', Integer )}
   end
 
   def test_case_equality_check_multiple_arguments
-    assert( @foo.case_equality_check_multiple_arguments( 1, 2 ))
+    assert( check( 1, 2, Integer ))
   end
 
   def test_case_equality_check_multiple_arguments_raises_exception
-    assert_raises( Simplecheck::CheckFailed ){ @foo.case_equality_check_multiple_arguments( 1, '2' )}
+    assert_raises( Simplecheck::CheckFailed ){ check( 1, '2', Integer )}
   end
 
   def test_block_check_true
-    assert( @foo.block_check( 2 )) 
+    assert( check( 1 ){ |n| n.odd? }) 
   end
 
   def test_block_check_raises_exception
-    assert_raises( Simplecheck::CheckFailed ){ @foo.block_check( 1 )}
+    assert_raises( Simplecheck::CheckFailed ){ check( 1 ){ |n| n.even? }}
   end
 
   def test_block_check_multiple_arguments_true
-    assert( @foo.block_check_multiple_arguments( 2, 2, 2 ))
+    assert( check( 1, 1, 1 ){ |n| n.odd? })
   end
 
   def test_block_check_multiple_arguments_exception
-    assert_raises( Simplecheck::CheckFailed ){ @foo.block_check_multiple_arguments( 2, 2, 1 )}
+    assert_raises( Simplecheck::CheckFailed ){ check( 1, 1, 2 ){ |n| n.odd? }}
   end
 
   def test_lambda_argument_check_true
-    assert( @foo.lambda_argument_check( 2 ))
+    assert( check( 1, ->(n){ n.odd? }))
   end
 
   def test_lambda_argument_check_raises_exception
-    assert_raises( Simplecheck::CheckFailed ){ @foo.lambda_argument_check( 1 )}
+    assert_raises( Simplecheck::CheckFailed ){ check( 2, ->(n){ n.odd? })}
   end
 
   def test_lambda_block_check_true
-    assert( @foo.lambda_block_check( 2 ))
+    assert( check( 1, &->(n){ n.odd? }))
   end
 
   def test_lambda_block_check_raises_exception
-    assert_raises( Simplecheck::CheckFailed ){ @foo.lambda_block_check( 1 )}
+    assert_raises( Simplecheck::CheckFailed ){ check( 2, &->(n){ n.odd? })}
   end
 end
